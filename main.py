5a0ac7eb35877b74ba9c2269143a328c4d95b94b
codedmachine111 <charlesbabbage1709@gmail.com>
2024-05-17 20:10:35 +0530

    feat: vector store

--- main.py
+++ main.py
@@ -1,34 +1,107 @@
+import os
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
 from process_input import processInput
+from dotenv import load_dotenv
 
-# SIDEBAR
-with st.sidebar:
-    st.title('Abridge')
-    st.markdown('''
-        ## About
-        Chat with your all your pdf files with ease!
-    ''')
-    add_vertical_space(5)
-    
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+import uuid
+from pinecone import Pinecone, ServerlessSpec
+
+# Load environment variables
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+index_name = "pdf-index"
+
+# Ensure the index exists
+if index_name not in pc.list_indexes().names():
+  pc.create_index(
+      name=index_name,
+      dimension=768,
+      metric="cosine",
+      spec=ServerlessSpec(
+          cloud='aws', 
+          region='us-east-1'
+      ) 
+  )
+
+# Connect to the index
+index = pc.Index(index_name, host=os.getenv("PINECONE_HOST"))
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vectors = []
+
+    # Batch the text chunks to process in groups of 100
+    batch_size = 100
+    for i in range(0, len(text_chunks), batch_size):
+        batch = text_chunks[i:i+batch_size]
+        batch_vectors = embeddings.embed_documents(batch)
+        vectors.extend(batch_vectors)
+
+    # Prepare data for upsert
+    pinecone_vectors = [(str(uuid.uuid4()), vector) for vector in vectors]
+
+    max_batch_size = 50  # Adjust this size based on Pinecone's request size limit
+    for i in range(0, len(pinecone_vectors), max_batch_size):
+        batch = pinecone_vectors[i:i+max_batch_size]
+        index.upsert(vectors=batch)
+        
+    return pinecone_vectors
 
 def main():
-    st.header("Drop your PDF files here")
+    st.set_page_config(page_title="Abridge", page_icon=":brain:")
+    st.header("Chat with your all your pdf files with ease! :books:")
+    
+    input = st.text_input("Ask a question")
+    
+    # SIDEBAR
+    with st.sidebar:
+        st.title('Abridge')
+        st.markdown('''
+            ## About
+            Chat with your all your pdf files with ease!
+        ''')
+        add_vertical_space(3)
+        st.divider()
+        st.header("Your files")
+        
+        uploaded_files = st.file_uploader(type=['pdf', 'zip'], label="Click below or drag and drop your files to upload!", accept_multiple_files=True)
+        
+        if 'processed_files' not in st.session_state:
+            st.session_state.processed_files = []
 
-    # Take input files
-    uploaded_file = st.file_uploader(type=['pdf', 'zip'], label="Click below or drag and drop your files to upload!")
+        if st.button("Process"):
+            # Process the input files accordingly
+            if uploaded_files:
+                new_files = [file for file in uploaded_files if file.name not in st.session_state.processed_files]
+                
+                if new_files:
+                    with st.status("Please wait as we process your input files...", expanded=True) as status:
+                        st.write("Extracting data from Documents...")
+                        for new_file in new_files:
+                            # Extract data
+                            text_chunks, table_chunks = processInput(new_file)
+                            status.update(label="Successfully Chunked data!", state="running", expanded=True)
 
-    # Process the input files accordingly
-    if(uploaded_file):   
-        # Extract text and tables
-        with st.status("Please wait as we processing your input files...", expanded=True) as status:
-            st.write("Extracting data from Documents...")
-            text_chunks = processInput(uploaded_file)
-            status.update(label="Processing done successfully!", state="complete", expanded=False)
+                            # Convert to embeddings
+                            st.write("Converting text to embeddings...")
+                            embeddings = get_vector_store(text_chunks[0])
+                            status.update(label="Successfully embedded chunks!", state="running", expanded=True)
 
-        st.write(text_chunks)
-    else:
-        st.write('''No pdf uploaded''')
+                            st.session_state.processed_files.append(new_file.name)
+                        status.update(label="Processing Complete!", state="complete", expanded=False)
+                else:
+                    st.write("All selected files have been processed already.")
+            else:
+                st.write("No PDFs uploaded.")
 
 if __name__ == '__main__':
     main()
--- process_input.py
+++ process_input.py
@@ -1,33 +1,75 @@
 import zipfile
 from PyPDF2 import PdfReader
-from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
+import camelot
+import tempfile
+import re
+from io import BytesIO
+
+def prune_text(text):
+    def replace_cid(match):
+        return ''
+
+    # Regular expression to find all (cid:x) patterns
+    cid_pattern = re.compile(r'\(cid:(\d+)\)')
+    pruned_text = re.sub(cid_pattern, replace_cid, text)
+    return pruned_text
 
 def processInput(file):
-    if(file==None):
-        print("Could not upload files! Try again!")
-        return
+    '''
+        Processes the input file based on file type
+    '''
+    text_chunks = []
+    table_chunks = []
+    if file is None:
+        st.error("Could not upload files! Try again!")
+        return []
     else:
-        chunks = []
-        if(file.type == "application/x-zip-compressed"):
+        if file.type == "application/x-zip-compressed":
             # Extract PDF files from zip
-            with zipfile.ZipFile(file, "r") as z:
-                z.extractall("./data/")
-        elif(file.type == "application/pdf"):
-            # Read text from the pdf
-            pdf_info = PdfReader(file)
-            
-            # Append all pdf text data into a string
-            text = ""
-            for page in pdf_info.pages:
-                text += page.extract_text()
-            
-            # Split the text into chunks
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len)
-            chunks = text_splitter.split_text(text)  
+            with zipfile.ZipFile(BytesIO(file.read()), "r") as z:
+                for pdf_file in z.namelist():
+                    with z.open(pdf_file) as pdf:
+                        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_pdf:
+                            temp_pdf.write(pdf.read())
+                            temp_pdf.seek(0)
+                            text, tables = extract_text_and_tables(temp_pdf.name)
+                            text_chunks += text
+                            table_chunks += tables
+        elif file.type == "application/pdf":
+            # Extract Text and Tables
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_pdf:
+                temp_pdf.write(file.read())
+                temp_pdf.seek(0)
+                text, tables = extract_text_and_tables(temp_pdf.name)
+                text_chunks += text
+                table_chunks += tables
         else:
-            print("Please upload a PDF or a zip file.")
-            return          
-    return chunks
+            st.error("Please upload a PDF or a zip file.")
 
+    return text_chunks, table_chunks
+
+def extract_text_and_tables(pdf_path):
+    '''
+        Takes one pdf file path as input and returns chunked text and tables
+    '''
+    chunks_per_pdf = []
+    all_table_chunks = []
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len)
+    pdf_info = PdfReader(pdf_path)
+    pdf_chunks = []
+    for page in pdf_info.pages:
+        text = page.extract_text()
+        if text:
+            pdf_chunks.extend(text_splitter.split_text(text))
+    chunks_per_pdf.append(pdf_chunks)
+
+    # Extract tables
+    # tables = camelot.read_pdf(pdf_path, pages='all', flavor='stream')
+    # for table in tables:
+    #     table_df = table.df
+    #     table_text = table_df.to_string(index=False, header=False)
+    #     all_table_chunks.extend(text_splitter.split_text(table_text))
+
+    return chunks_per_pdf, all_table_chunks
